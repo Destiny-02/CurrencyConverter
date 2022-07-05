1e1ff1eb05b98eb6614ae1626389866a4bc13bdd
Destiny <destinylinz@gmail.com>
2022-07-06 00:49:28 +1200

    show all currencies and tick the ones from the dropdown

--- app/src/main/java/com/desti/currencyconverter/CustomiseActivity.java
+++ app/src/main/java/com/desti/currencyconverter/CustomiseActivity.java
@@ -10,9 +10,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -24,18 +24,19 @@ public class CustomiseActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private CurrencyListAdapter currencyListAdapter;
     private List<CurrencyModel> currencyModelList;
+    private List<String> dropdownOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
-        String[] dropdownOptions;
         if (extras != null) {
-            dropdownOptions = extras.getStringArray("dropdown");
+            dropdownOptions = Arrays.asList(extras.getStringArray("dropdown"));
         } else {
-            dropdownOptions = new String[] {};
+            dropdownOptions = new ArrayList<>();
         }
-        currencyModelList = currencyStringToModel(dropdownOptions);
+        initCurrencyList();
+
         setContentView(R.layout.activity_customise);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -49,8 +50,8 @@ public class CustomiseActivity extends AppCompatActivity {
                 DividerItemDecoration.VERTICAL));
     }
 
-    private String[] getCurrencyList() {
-        String[] symbolsArray = new String[] {};
+    private void initCurrencyList() {
+        currencyModelList = new ArrayList<>();
         try {
             OkHttpClient client = new OkHttpClient().newBuilder().build();
             String url = "https://api.exchangerate.host/symbols";
@@ -61,18 +62,19 @@ public class CustomiseActivity extends AppCompatActivity {
             Response response = client.newCall(request).execute();
             String responseString = response.body().string();
             JSONObject json = new JSONObject(responseString).getJSONObject("symbols");
-            ArrayList<String> keys = new ArrayList<>();
             String key;
             for (Iterator<String> it = json.keys(); it.hasNext(); ) {
                 key = it.next();
-                keys.add(key);
+                boolean isChecked = false;
+                if (dropdownOptions.contains(key)) {
+                    isChecked = true;
+                }
+                currencyModelList.add(new CurrencyModel(key, isChecked));
             }
-            symbolsArray = keys.toArray(new String[0]);
         } catch (Exception e) {
             Toast.makeText(CustomiseActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
-            return new String[] {};
+            return;
         }
-        return symbolsArray;
     }
 
     @Override
@@ -81,11 +83,19 @@ public class CustomiseActivity extends AppCompatActivity {
         return true;
     }
 
-    public List<CurrencyModel> currencyStringToModel(String[] currencyStrings) {
+    public List<CurrencyModel> currencyStringsToModels(String[] currencyStrings) {
         List<CurrencyModel> currencyModels = new ArrayList<>();
         for (String s : currencyStrings) {
             currencyModels.add(new CurrencyModel(s, false));
         }
         return currencyModels;
+    }
+
+    public String[] currencyModelsToStrings(List<CurrencyModel> currencyModels) {
+        String[] currencyStrings = new String[currencyModels.size()];
+        for (int i=0; i<currencyModels.size(); i++) {
+            currencyStrings[i] = currencyModels.get(i).getCurrency();
+        }
+        return currencyStrings;
     }
 }

8615835d554fccb9671efbff313f73edbdc07fa8
Destiny <destinylinz@gmail.com>
2022-07-05 16:43:21 +1200

    add and populate recyclerview on customise activity

--- app/src/main/java/com/desti/currencyconverter/CustomiseActivity.java
+++ app/src/main/java/com/desti/currencyconverter/CustomiseActivity.java
@@ -1,16 +1,32 @@
 package com.desti.currencyconverter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class CustomiseActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CurrencyListAdapter currencyListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customise);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        View v = findViewById(android.R.id.content).getRootView();
+        recyclerView = v.findViewById(R.id.currency_recycler_view);
+        currencyListAdapter = new CurrencyListAdapter(getCurrencyList());
+        recyclerView.setAdapter(currencyListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private String[] getCurrencyList() {
+        return new String[] {"apple", "chicken", "penguin"};
     }
 
     @Override
